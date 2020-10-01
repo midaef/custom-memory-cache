90ceeb453920d9be79f1b7c182aa8c8c9a58c973
Midaef <36952775+midaef@users.noreply.github.com>
2020-10-01 14:37:28 +0800

    update(cache): add read and write

--- cache/cache.go
+++ cache/cache.go
@@ -1,17 +1,65 @@
 package cache
 
-import "sync"
+import (
+	"errors"
+	"sync"
+	"time"
+
+	"github.com/midaef/custom-memory-cache/models"
+)
+
+var (
+	ErrKeyNotFound     = errors.New("custom-memory-cache: Key not found")
+	ErrCacheIsNotAlive = errors.New("custom-memory-cache: Cache is not alive")
+)
 
 // Cache ...
 type Cache struct {
-	DataMutex *sync.Mutex
-	HashSize  uint16
-	Data      map[string]string
+	DataMutex  *sync.Mutex
+	Size       uint16
+	GCInterval time.Duration
+	Data       map[string]*models.Data
 }
 
-func NewCache() *Cache {
-	return &Cache{
-		DataMutex: new(sync.Mutex),
-		HashSize:  1024,
+func NewCache(size uint16, gcInterval time.Duration) *Cache {
+	if gcInterval > 0 {
+
 	}
+	cache := &Cache{
+		DataMutex:  new(sync.Mutex),
+		Data:       make(map[string]*models.Data, size),
+		Size:       size,
+		GCInterval: gcInterval,
+	}
+	return cache
+}
+
+func (cache *Cache) Write(key string, value interface{}, lifeTime time.Duration) {
+	var lfTime int64
+	if lifeTime > 0 {
+		lfTime = time.Now().Add(lifeTime).UnixNano()
+	}
+	cache.DataMutex.Lock()
+	cache.Data[key] = &models.Data{
+		Data:     value,
+		LifeTime: lfTime,
+		Created:  time.Now(),
+		Alive:    true,
+	}
+	cache.DataMutex.Unlock()
+}
+
+func (cache *Cache) Read(key string) (interface{}, error) {
+	cache.DataMutex.Lock()
+	defer cache.DataMutex.Unlock()
+	data, found := cache.Data[key]
+	if !found {
+		return nil, ErrKeyNotFound
+	}
+	if data.LifeTime > 0 {
+		if time.Now().UnixNano() > data.LifeTime {
+			return nil, ErrCacheIsNotAlive
+		}
+	}
+	return data.Data, nil
 }
--- models/data.go
+++ models/data.go
@@ -1,5 +1,11 @@
 package models
 
+import "time"
+
 // Data
 type Data struct {
+	Data     interface{}
+	Alive    bool
+	Created  time.Time
+	LifeTime int64
 }
